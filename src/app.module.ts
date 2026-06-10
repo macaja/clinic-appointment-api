3c06641e993ddef1a21b7351580bbfe39f454b42
Mauricio Cardona <mauricio.cardonajaramillo@estimateone.com>
2026-06-11 02:02:41 +1000

    feat(http): add Swagger and wire DI ports to adapters

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,70 @@
 import { Module } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { HealthController } from './infra/http/health.controller';
+import {
+  AppointmentsController,
+  CREATE_APPOINTMENT_USE_CASE,
+  LIST_ALL_APPOINTMENTS_USE_CASE,
+} from './infra/http/appointments.controller';
+import {
+  CliniciansController,
+  LIST_CLINICIAN_APPOINTMENTS_USE_CASE,
+} from './infra/http/clinicians.controller';
+import { createDb, SQLITE_DB } from './infra/persistence/sqlite/db';
+import { SqliteAppointmentRepository } from './infra/persistence/sqlite/sqlite-appointment.repository';
+import { SqlitePeopleRepository } from './infra/persistence/sqlite/sqlite-people.repository';
+import { APPOINTMENT_REPOSITORY } from './application/ports/appointment-repository.port';
+import { PEOPLE_REPOSITORY } from './application/ports/people-repository.port';
+import { CLOCK, SystemClock } from './application/ports/clock.port';
+import { CreateAppointmentUseCase } from './application/use-cases/create-appointment.use-case';
+import { ListClinicianAppointmentsUseCase } from './application/use-cases/list-clinician-appointments.use-case';
+import { ListAllAppointmentsUseCase } from './application/use-cases/list-all-appointments.use-case';
+import { RolesGuard } from './infra/http/auth/roles.guard';
+import Database from 'better-sqlite3';
+import { AppointmentRepository } from './application/ports/appointment-repository.port';
+import { PeopleRepository } from './application/ports/people-repository.port';
+import { Clock } from './application/ports/clock.port';
 
 @Module({
-  controllers: [HealthController],
+  controllers: [HealthController, AppointmentsController, CliniciansController],
+  providers: [
+    Reflector,
+    {
+      provide: SQLITE_DB,
+      useFactory: () => createDb(),
+    },
+    {
+      provide: APPOINTMENT_REPOSITORY,
+      useFactory: (db: Database.Database) => new SqliteAppointmentRepository(db),
+      inject: [SQLITE_DB],
+    },
+    {
+      provide: PEOPLE_REPOSITORY,
+      useFactory: (db: Database.Database) => new SqlitePeopleRepository(db),
+      inject: [SQLITE_DB],
+    },
+    {
+      provide: CLOCK,
+      useClass: SystemClock,
+    },
+    {
+      provide: CREATE_APPOINTMENT_USE_CASE,
+      useFactory: (ar: AppointmentRepository, pr: PeopleRepository, cl: Clock) =>
+        new CreateAppointmentUseCase(ar, pr, cl),
+      inject: [APPOINTMENT_REPOSITORY, PEOPLE_REPOSITORY, CLOCK],
+    },
+    {
+      provide: LIST_CLINICIAN_APPOINTMENTS_USE_CASE,
+      useFactory: (ar: AppointmentRepository, cl: Clock) =>
+        new ListClinicianAppointmentsUseCase(ar, cl),
+      inject: [APPOINTMENT_REPOSITORY, CLOCK],
+    },
+    {
+      provide: LIST_ALL_APPOINTMENTS_USE_CASE,
+      useFactory: (ar: AppointmentRepository, cl: Clock) => new ListAllAppointmentsUseCase(ar, cl),
+      inject: [APPOINTMENT_REPOSITORY, CLOCK],
+    },
+    RolesGuard,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { DomainExceptionFilter } from './infra/http/domain-exception.filter';
 import { LoggingInterceptor } from './infra/http/logging.interceptor';
@@ -13,7 +14,13 @@ async function bootstrap(): Promise<void> {
   app.useGlobalFilters(new DomainExceptionFilter());
   app.useGlobalInterceptors(new LoggingInterceptor());
 
-  // E6: Swagger setup registered here
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Clinic Appointment API')
+    .setDescription('Book appointments; clinician & admin listings')
+    .setVersion('1.0')
+    .addApiKey({ type: 'apiKey', name: 'X-Role', in: 'header' }, 'X-Role')
+    .build();
+  SwaggerModule.setup('docs', app, SwaggerModule.createDocument(app, swaggerConfig));
 
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
